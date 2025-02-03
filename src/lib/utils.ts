a29a8ebe9ec3d2d060b24e98ccf04b046ed42ca9
rezashahnazar <reza.shahnazar@gmail.com>
2025-02-03 10:19:35 +0330

    Remove unused lastArgs variable in debounce utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,7 +15,6 @@ export function debounce<T extends (...args: Parameters<T>) => ReturnType<T>>(
   options: DebounceOptions = {}
 ): (...args: Parameters<T>) => void {
   let timeoutId: ReturnType<typeof setTimeout> | undefined;
-  let lastArgs: Parameters<T> | undefined;
 
   return function debounced(...args: Parameters<T>) {
     // Using arrow function to preserve this context
